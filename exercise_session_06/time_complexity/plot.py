65aaba5472f2a20fb8c20a87f9b32fed55354953
Dylan Baumgartner <djb.21@live.com>
2022-04-07 22:08:24 +0200

    ex6

--- exercise_session_06/time_complexity/plot.py
+++ exercise_session_06/time_complexity/plot.py
@@ -1,27 +1,17 @@
-#!/usr/bin/env python
-import re, sys
-import matplotlib.pyplot as plt
 import numpy as np
-x=[100,500,1000,5000,10000,20000,50000]
-y=[]
-for line in re.finditer(
-    r"Duration = ([0-9].[0-9]+e[+,-][0-9]+) s",
-    sys.stdin.read(), re.MULTILINE):
-    y.append(float(line.group(1)))
-    print("{}".format(line.group(1)))
+import matplotlib.pyplot as plt
 
-x = np.array(x)
-y = np.array(y)
+data = np.genfromtxt('./speedup.txt')
+for ncpus,time in data:
+    if ncpus == 1:
+        norm = time
+        
+data[:,1] = norm/data[:,1]
 
-xx = np.arange(0,50000,10)
-yfit = xx**2.0 * y[-1] / x[-1]**2.0
-
-plt.plot(xx,yfit,label='fit')
-plt.plot(x,y,label='data',marker='x',linestyle='None')
-plt.gca().set_xscale('linear')
-plt.gca().set_yscale('linear')
-plt.xlabel('N')
-plt.ylabel('Runtime [s]')
-
-plt.legend()
-plt.savefig('speedup.png',dpi=350)
+fig,ax = plt.subplots()
+ax.plot(data[:,0],data[:,1],'s',label = 'real speedup')
+ax.plot(np.arange(24),np.arange(24),label = 'ideal speedup')
+ax.legend()
+ax.set_xlabel('\#CPU')
+ax.set_ylabel('Speedup')
+fig.savefig('./speedup.png',dpi=350)
